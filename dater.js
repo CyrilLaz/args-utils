39dc2b9412852a9d14040181fdc0c1ef4c98101b
Cyril_Lazar <klazar@mail.ru>
2024-02-29 23:13:46 +0700

    feature: сделал методы add и sub

--- dater.js
+++ dater.js
@@ -1,7 +1,13 @@
 /**
+ * @callback fn
+ * @param {{ year?:number, month?:number, date?:number}}
+ * @return {string}
+ */
+/**
  * @param {{year?:boolean, month?:boolean, date?:boolean}}
  * @return {string}
  */
+
 module.exports.current = ({ date, month, year } = {}) => {
   const nowDate = new Date();
   switch (true) {
@@ -14,4 +20,30 @@ module.exports.current = ({ date, month, year } = {}) => {
     default:
       return nowDate.toISOString();
   }
+};
+
+/** @type {fn} */
+module.exports.add = ({ date = 0, month = 0, year = 0 }) => {
+  const nowDate = new Date();
+
+  const nowYear = nowDate.getFullYear();
+  const nowMonth = nowDate.getMonth();
+  const nowDay = nowDate.getDate();
+
+  return new Date(
+    nowDate.setFullYear(nowYear + year, nowMonth + month, nowDay + date)
+  ).toISOString();
+};
+
+/** @type {fn} */
+module.exports.sub = ({ date = 0, month = 0, year = 0 }) => {
+  const nowDate = new Date();
+
+  const nowYear = nowDate.getFullYear();
+  const nowMonth = nowDate.getMonth();
+  const nowDay = nowDate.getDate();
+
+  return new Date(
+    nowDate.setFullYear(nowYear - year, nowMonth - month, nowDay - date)
+  ).toISOString();
 };
